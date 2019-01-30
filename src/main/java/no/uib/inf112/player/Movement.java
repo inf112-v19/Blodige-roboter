47e8350846b1588928b9d160ec1d12b576955678
rikkeaas <rikke.aas@student.uib.no>
2019-01-30 19:29:08 +0100

    Implimented draw-method in Deck-class. Moved movement-enum to different file.

---
+++ src/main/java/no/uib/inf112/player/Movement.java
@@ -0,0 +1,17 @@
+package no.uib.inf112.player;
+
+/**
+ * All legal movements that robots can do.
+ */
+
+public enum Movement {
+
+        RIGHT_TURN,
+        LEFT_TURN,
+        U_TURN,
+        MOVE_1,
+        MOVE_2,
+        MOVE_3,
+        BACK_UP
+}
+
