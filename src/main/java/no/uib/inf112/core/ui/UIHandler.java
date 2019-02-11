93abe6258ece9f8f5ecd15e103923890f67aeb9f
Karl Henrik Elg <klink100@gmail.com>
2019-02-11 12:18:54 +0100

    Center UI on the bottom center of the screen

--- src/main/java/no/uib/inf112/core/ui/UIHandler.java
+++ src/main/java/no/uib/inf112/core/ui/UIHandler.java
@@ -7,11 +7,9 @@ import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.HorizontalGroup;
-import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import no.uib.inf112.core.RoboRally;
@@ -23,7 +21,7 @@ public class UIHandler implements Disposable {
 
     private final Skin skin;
     private final Table table;
-    private Stage stage;
+    private final Stage stage;
 
 
     public static final String SKIN_NAME = "neutralizer-ui-skin";
@@ -38,24 +36,25 @@ public class UIHandler implements Disposable {
     private static final TextureRegion DAMAGE_TOKEN_TEXTURE;
 
     static {
-        UI_BACKGROUND_TEXTURE = createTempTexture(100, Color.BLACK, false);
-        CARDS_TEXTURE = createTempTexture(100, Color.BLUE, false);
-        POWER_DOWN_TEXTURE = createTempTexture(51, Color.RED, true);
-        LIFE_TOKEN_TEXTURE = createTempTexture(25, Color.GREEN, true);
-        DAMAGE_TOKEN_TEXTURE = createTempTexture(25, Color.YELLOW, true);
+        UI_BACKGROUND_TEXTURE = createTempRectTexture(1, 1, new Color(0.145f, 0.145f, 0.145f, 0.9f));
+        CARDS_TEXTURE = createTempRectTexture(56, 90, Color.BLUE);
+        POWER_DOWN_TEXTURE = createTempCircleTexture(41, Color.RED);
+        LIFE_TOKEN_TEXTURE = createTempCircleTexture(25, Color.GREEN);
+        DAMAGE_TOKEN_TEXTURE = createTempCircleTexture(19, Color.YELLOW);
 
     }
 
-    private static TextureRegion createTempTexture(int size, Color color, boolean circle) {
-        final Pixmap pixmap = new Pixmap(size, size, Pixmap.Format.RGB888);
+    private static TextureRegion createTempCircleTexture(int size, Color color) {
+        final Pixmap pixmap = new Pixmap(size, size, Pixmap.Format.RGBA8888);
         pixmap.setColor(color);
+        pixmap.fillCircle(pixmap.getWidth() / 2, pixmap.getWidth() / 2, size / 2);
+        return new TextureRegion(new Texture(pixmap));
+    }
 
-        if (circle) {
-            pixmap.fillCircle(pixmap.getWidth() / 2, pixmap.getWidth() / 2, size / 2);
-        }
-        else {
-            pixmap.fill();
-        }
+    private static TextureRegion createTempRectTexture(int width, int height, Color color) {
+        final Pixmap pixmap = new Pixmap(width, height, Pixmap.Format.RGBA8888);
+        pixmap.setColor(color);
+        pixmap.fill();
         return new TextureRegion(new Texture(pixmap));
     }
 
@@ -63,59 +62,56 @@ public class UIHandler implements Disposable {
         stage = new Stage(new ScreenViewport());
         RoboRally.getInputMultiplexer().addProcessor(stage);
 
+        stage.setDebugAll(true);
+
         skin = new Skin(Gdx.files.internal(SKIN_JSON_FILE));
+        table = new Table(skin);
 
-        table = new Table();
+        create();
+        resize();
+
+    }
+
+    /**
+     * Initiate ui
+     */
+    private void create() {
+
         stage.addActor(table);
+        table.setTransform(false);
 
-        table.setDebug(true, true);
-        table.setBackground(new TextureRegionDrawable(UI_BACKGROUND_TEXTURE));
-        table.pad(5);
+//        table.setBackground(new TextureRegionDrawable(UI_BACKGROUND_TEXTURE));
 
-
-//        TextButton button = new TextButton("cards", skin);
-//        button.addListener(new ClickListener() {
-//            @Override
-//            public void clicked(InputEvent event, float x, float y) {
-//                RoboRally.round(); // user has placed his "cards"
-//            }
-//        });
-//
-////        button.setOrigin(Align.left);
-//        table.add(button).pad(10);
-
-        HorizontalGroup topRow = new HorizontalGroup();
-        table.add(topRow);
+        Table topRow = new Table(skin);
+        table.add(topRow).expandX().fillX().align(Align.center);
+        table.row();
 
         HorizontalGroup lifeTokens = new HorizontalGroup();
-        topRow.addActor(lifeTokens);
-        lifeTokens.space(25);
-
+        topRow.add(lifeTokens).expandX().fillX().align(Align.bottomLeft);
+        lifeTokens.space(5);
         for (int i = 0; i < 3; i++) {
             lifeTokens.addActor(new ImageButton(new TextureRegionDrawable(LIFE_TOKEN_TEXTURE)));
         }
-        topRow.addActor(new ImageButton(new TextureRegionDrawable(POWER_DOWN_TEXTURE)));
-        topRow.space(350);
-        topRow.pad(2);
-        table.row();
+
+        Container<ImageButton> power = new Container<>(new ImageButton(new TextureRegionDrawable(POWER_DOWN_TEXTURE)));
+        topRow.add(power).fillX().align(Align.right);
+
 
         HorizontalGroup damageRow = new HorizontalGroup();
-        table.add(damageRow);
+        table.add(damageRow).expandX().fillX().align(Align.left).padTop(5).padBottom(5);
         table.row();
+
         damageRow.space(5);
         for (int i = 0; i < 10; i++) {
             damageRow.addActor(new ImageButton(new TextureRegionDrawable(DAMAGE_TOKEN_TEXTURE)));
         }
 
         HorizontalGroup cardsRow = new HorizontalGroup();
-        table.add(cardsRow);
+        table.add(cardsRow).align(Align.left);
         cardsRow.space(5);
         for (int i = 0; i < 5; i++) {
             cardsRow.addActor(new ImageButton(new TextureRegionDrawable(CARDS_TEXTURE)));
         }
-
-        resize();
-
     }
 
     public void update() {
@@ -126,9 +122,8 @@ public class UIHandler implements Disposable {
     public void resize() {
         stage.getViewport().update(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), true);
 
-        table.setHeight(Gdx.graphics.getHeight() / 2f);
-        table.setWidth(Gdx.graphics.getWidth());//) - (Gdx.graphics.getWidth() / 10f)
-
+        table.setX(Gdx.graphics.getWidth() / 2f);
+        table.setHeight(table.getPrefHeight() + 25);
     }
 
     @Override
