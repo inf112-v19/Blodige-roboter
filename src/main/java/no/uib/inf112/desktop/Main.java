e291618dba916348253707b9fa99d9f366cd4d7c
Karl Henrik Elg <klink100@gmail.com>
2019-02-11 12:17:36 +0100

    Expand default window size to make room for UI

--- src/main/java/no/uib/inf112/desktop/Main.java
+++ src/main/java/no/uib/inf112/desktop/Main.java
@@ -6,11 +6,12 @@ import no.uib.inf112.core.RoboRally;
 
 
 public class Main {
+
     public static void main(String[] args) {
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
         cfg.title = "Robo Rally";
-        cfg.width = 480;
-        cfg.height = 320;
+        cfg.width = 1280;
+        cfg.height = 720;
         cfg.samples = 16; //max out the samples as this isn't a very heavy game.
 
         new LwjglApplication(new RoboRally(), cfg);
