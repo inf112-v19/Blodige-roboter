f0e7af6bd04f93bfd93d044475125d6a975b3929
rikkeaas <rikke.aas@student.uib.no>
2019-02-12 17:51:01 +0100

    Finished test for Inverse() in Direction

--- src/test/java/no/uib/inf112/core/player/DirectionTest.java
+++ src/test/java/no/uib/inf112/core/player/DirectionTest.java
@@ -2,8 +2,7 @@ package no.uib.inf112.core.player;
 
 import org.junit.Test;
 
-import static no.uib.inf112.core.player.Direction.NORTH;
-import static no.uib.inf112.core.player.Direction.SOUTH;
+import static no.uib.inf112.core.player.Direction.*;
 import static org.junit.Assert.*;
 
 public class DirectionTest {
@@ -17,5 +16,15 @@ public class DirectionTest {
         assertEquals(SOUTH, dir.inverse());
     }
 
+    @Test
+    public void NORTHInvertedTwiceShouldReturnNORTH() {
+        Direction dir = NORTH;
+        assertEquals(NORTH, dir.inverse().inverse());
+    }
 
+    @Test
+    public void EASTInvertedShouldReturnWEST() {
+        Direction dir = EAST;
+        assertEquals(WEST, dir.inverse());
+    }
 }
