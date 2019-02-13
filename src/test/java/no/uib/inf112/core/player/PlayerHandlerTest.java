0cf3207f1cdafde997acd5653cb0a156baa57931
Daniel Berge <daniel@berge.us>
2019-02-13 21:57:55 +0100

    Added playerhandlertest and todos

---
+++ src/test/java/no/uib/inf112/core/player/PlayerHandlerTest.java
@@ -0,0 +1,7 @@
+package no.uib.inf112.core.player;
+
+public class PlayerHandlerTest {
+
+    //TODO Test if constructor works as expected.
+    //TODO Test if generatePlayers() words as expected.
+}
