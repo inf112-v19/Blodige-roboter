9d554c92684afc32687da81ae3a368cdef94c9e6
rikkeaas <rikke.aas@student.uib.no>
2019-02-13 10:13:27 +0100

    Testet moving robot forwards

--- src/test/java/no/uib/inf112/core/player/RobotTest.java
+++ src/test/java/no/uib/inf112/core/player/RobotTest.java
@@ -1,14 +1,55 @@
 package no.uib.inf112.core.player;
 
+import jdk.nashorn.internal.objects.annotations.Setter;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class RobotTest {
 
-    @Test
-    public void move() {
-        Robot testBot = new Robot(0, 0, Direction.NORTH, true);
+    private Robot testBot;
+    private int roboX;
+    private int roboY;
+
+    @Before
+    public void setup() {
+        testBot = new Robot(5, 5, Direction.NORTH, true);
+        roboX = testBot.getX();
+        roboY = testBot.getY();
 
     }
+
+    @Test
+    public void movingOneFacingNorthShouldNotChangeX() {
+        testBot.move(Movement.MOVE_1);
+        assertEquals(roboX, testBot.getX());
+    }
+
+    @Test
+    public void movingOneFacingNorthShouldIncrementY() {
+        testBot.move(Movement.MOVE_1);
+        assertEquals(roboY + 1, testBot.getY());
+    }
+
+    @Test
+    public void movingThreeFacingNorthShouldIncreaseYWithThree() {
+        testBot.move(Movement.MOVE_3);
+        assertEquals(roboY + 3, testBot.getY());
+    }
+
+    @Test
+    public void movingTwoFacingEastShouldNotChangeY() {
+        testBot.setDirection(Direction.EAST);
+        testBot.move(Movement.MOVE_2);
+        assertEquals(roboY, testBot.getY());
+    }
+
+    @Test
+    public void movingTwoFacingEastShouldIncreaseXWithTwo() {
+        testBot.setDirection(Direction.EAST);
+        testBot.move(Movement.MOVE_2);
+        assertEquals(roboX + 2, testBot.getX());
+    }
+
 }
