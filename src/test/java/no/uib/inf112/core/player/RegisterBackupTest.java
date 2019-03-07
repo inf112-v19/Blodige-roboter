132cd2009553a7fcf78ef26c9fe9538975ba2530
Kristian Aksland <K.Aksland@student.uib.no>
2019-03-07 21:32:11 +0100

    Fixed small compile error in RegisterBackupTest

--- src/test/java/no/uib/inf112/core/player/RegisterBackupTest.java
+++ src/test/java/no/uib/inf112/core/player/RegisterBackupTest.java
@@ -10,7 +10,7 @@ public class RegisterBackupTest {
     //TODO #77 this should be tested way more.
     @Test
     public void settingBackupShouldUpdateBackup() {
-        Player testPlayer = new Player(1, 1, Direction.NORTH, true);
+        Player testPlayer = new Player(1, 1, Direction.NORTH);
         Vector2Int newBackup = new Vector2Int(0, 0);
         testPlayer.setBackup(newBackup.x, newBackup.y);
         assertEquals(newBackup, testPlayer.getBackup());
