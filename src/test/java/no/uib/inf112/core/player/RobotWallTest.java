7ec5462afcb303783564b4ec0d3fbee0418a22c4
rikkeaas <rikrikaas@gmail.com>
2019-03-18 17:30:50 +0100

    Added setup method to class to test wall interaction

--- src/test/java/no/uib/inf112/core/player/RobotWallTest.java
+++ src/test/java/no/uib/inf112/core/player/RobotWallTest.java
@@ -3,18 +3,30 @@ package no.uib.inf112.core.player;
 import no.uib.inf112.core.GameGraphics;
 import no.uib.inf112.core.RoboRally;
 import no.uib.inf112.desktop.TestGraphics;
+import org.junit.Before;
 import org.junit.BeforeClass;
 
 import java.io.File;
 
 public class RobotWallTest extends TestGraphics {
 
+    private static Player player;
+    private static Robot testBot;
     private static RoboRally roboRally;
 
     @BeforeClass
     public static void beforeClass() {
         roboRally = GameGraphics.getRoboRally();
         roboRally.changeMap(TEST_MAP_FOLDER + File.separatorChar + "player_wall_test_map.tmx");
+    }
+
+    @Before
+    public void setup() {
+        roboRally.getPlayerHandler().generateOnePlayer();
+        player = roboRally.getPlayerHandler().testPlayer();
+        testBot = player.getRobot();
+        testBot.teleport(0, 0);
+        player.setBackup(0, 0);
     }
 
 }
