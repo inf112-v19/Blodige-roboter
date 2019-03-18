b4be8dc65f7ab32a8f4f12282e03221dbbf09a3a
Elg <klink100@gmail.com>
2019-03-18 17:31:42 +0100

    Fix map not found

--- src/test/java/no/uib/inf112/core/player/RegisterBackupTest.java
+++ src/test/java/no/uib/inf112/core/player/RegisterBackupTest.java
@@ -3,14 +3,16 @@ package no.uib.inf112.core.player;
 import no.uib.inf112.core.GameGraphics;
 import no.uib.inf112.core.map.MapHandler;
 import no.uib.inf112.core.util.Vector2Int;
+import no.uib.inf112.desktop.TestGraphics;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class RegisterBackupTest {
+public class RegisterBackupTest extends TestGraphics {
 
 
     private MapHandler map = GameGraphics.getRoboRally().getCurrentMap();
+
     //TODO #77 this should be tested way more.
     @Test
     public void settingBackupShouldUpdateBackup() {
