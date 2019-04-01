bfdd702089b66569a9944a48cf4460f7d5ffbbc6
rikkeaas <rikrikaas@gmail.com>
2019-04-01 10:49:34 +0200

    Fixed tests after addition of laser layer

--- src/test/java/no/uib/inf112/core/map/tile/tiles/ConveyorTileTest.java
+++ src/test/java/no/uib/inf112/core/map/tile/tiles/ConveyorTileTest.java
@@ -2,8 +2,8 @@ package no.uib.inf112.core.map.tile.tiles;
 
 import no.uib.inf112.core.GameGraphics;
 import no.uib.inf112.core.RoboRally;
+import no.uib.inf112.core.map.MapHandler;
 import no.uib.inf112.core.player.AbstractPlayer;
-import no.uib.inf112.core.round.phase.ConveyorPhase;
 import no.uib.inf112.core.util.Direction;
 import no.uib.inf112.desktop.TestGraphics;
 import org.junit.Before;
@@ -17,29 +17,28 @@ import static org.junit.Assert.assertEquals;
 
 /**
  * Coordinates for impotant tiles on the test map for the testing of conveyors:
- * - (0,0) : single step north conveyor
- * - (0,1) : single step south conveyor
- * - (1,0) : singel step east conveyor
- * - (1,1) : singel step west conveyor
- * - (0,6) : singel step right turn conveyor (robot should be rotated)
+ * - (1,0) : single step north conveyor
+ * - (1,1) : single step south conveyor
+ * - (2,0) : singel step east conveyor
+ * - (2,1) : singel step west conveyor
+ * - (1,6) : singel step right turn conveyor (robot should be rotated)
  * - (5,0)-(5,1) : double step north conveyor
  * - (5,3)-(5,2) : double step south conveyor
  * - (7,1)-(6,1) : double step west conveyor
  * - (6,0)-(7,0) : double step east conveyor
  * - (6,2) : double step north conveyor moving onto normal board tile (robot should only move one step)
- * - (5,5) : double step north conveyor moving onto right turn (robot should be rotated)
+ * - (5,6) : double step right turn conveyor (robot should be rotated)
  */
 
 public class ConveyorTileTest extends TestGraphics {
 
+
     private static RoboRally roboRally;
-    private static ConveyorPhase testPhase;
     private AbstractPlayer testPlayer;
 
     @BeforeClass
     public static void beforeClass() {
         roboRally = GameGraphics.createRoboRally(TEST_MAP_FOLDER + File.separatorChar + "conveyor_tile_test_map.tmx", 1);
-        testPhase = new ConveyorPhase(0);
     }
 
     @Before
@@ -49,41 +48,49 @@ public class ConveyorTileTest extends TestGraphics {
     }
 
     private ConveyorTile getConveyorTile(int x, int y) {
-        return (ConveyorTile) roboRally.getCurrentMap().getTile("board", x, y);
+        return (ConveyorTile) roboRally.getCurrentMap().getTile(MapHandler.BOARD_LAYER_NAME, x, y);
     }
 
-    private void conveyorTileAction(int startX, int startY, int endX, int endY) {
+    private void conveyorTileAction(int startX, int startY) {
         testPlayer.teleport(startX, startY);
         ConveyorTile conveyor = getConveyorTile(startX, startY);
         conveyor.action(testPlayer);
-        assertEquals(endX, testPlayer.getX());
-        assertEquals(endY, testPlayer.getY());
     }
 
     @Test
-    public void singleStepNorthShouldMoveRobotOneStep() {
-        conveyorTileAction(0, 0, 0, 1);
+    public void singleStepNorthShouldChangeRobotsY() {
+        conveyorTileAction(1, 0);
+        assertEquals(1, testPlayer.getX());
+        assertEquals(1, testPlayer.getY());
     }
 
     @Test
-    public void singleStepSouthShouldMoveRobotOneStep() {
-        conveyorTileAction(0, 1, 0, 0);
+    public void singleStepSouthShouldChangeRobotsY() {
+        conveyorTileAction(1, 1);
+        assertEquals(1, testPlayer.getX());
+        assertEquals(0, testPlayer.getY());
     }
 
     @Test
-    public void singleStepWestShouldMoveRobotOneStep() {
-        conveyorTileAction(1, 1, 0, 1);
+    public void singleStepWestShouldChangeRobotsX() {
+        conveyorTileAction(2, 1);
+        assertEquals(1, testPlayer.getX());
+        assertEquals(1, testPlayer.getY());
     }
 
     @Test
-    public void singleStepEastShouldMoveRobotOneStep() {
-        conveyorTileAction(1, 0, 2, 0);
+    public void singleStepEastShouldChangeRobotsX() {
+        conveyorTileAction(2, 0);
+        assertEquals(3, testPlayer.getX());
+        assertEquals(0, testPlayer.getY());
     }
 
     @Test
     public void movingFromTurningConveyorShouldTurnRobot() {
         testPlayer.setDirection(Direction.NORTH);
-        conveyorTileAction(0, 6, 1, 6);
+        conveyorTileAction(1, 6);
+        assertEquals(2, testPlayer.getX());
+        assertEquals(6, testPlayer.getY());
         assertEquals(Direction.EAST, testPlayer.getDirection());
     }
 
