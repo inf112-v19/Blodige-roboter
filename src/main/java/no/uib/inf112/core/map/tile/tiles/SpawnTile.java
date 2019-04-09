acd01b8982d917b32d4e001997133018609be10d
Daniel Berge <daniel@berge.us>
2019-04-09 10:56:32 +0200

    Fixed spawntile

--- src/main/java/no/uib/inf112/core/map/tile/tiles/SpawnTile.java
+++ src/main/java/no/uib/inf112/core/map/tile/tiles/SpawnTile.java
@@ -3,28 +3,15 @@ package no.uib.inf112.core.map.tile.tiles;
 import no.uib.inf112.core.map.tile.TileGraphic;
 import no.uib.inf112.core.map.tile.api.AbstractTile;
 import no.uib.inf112.core.map.tile.api.DockableTile;
-import no.uib.inf112.core.map.tile.api.SingleDirectionalTile;
-import no.uib.inf112.core.util.Direction;
 import no.uib.inf112.core.util.Vector2Int;
 import org.jetbrains.annotations.NotNull;
 
-public class SpawnTile extends AbstractTile implements SingleDirectionalTile, DockableTile {
+public class SpawnTile extends AbstractTile implements DockableTile {
     private TileGraphic tg;
 
     public SpawnTile(@NotNull Vector2Int pos, @NotNull TileGraphic tg) {
         super(pos, tg);
         this.tg = tg;
-    }
-
-    @NotNull
-    @Override
-    public Direction getDirection() {
-        return null;
-    }
-
-    @Override
-    public void setDirection(@NotNull Direction direction) {
-
     }
 
     @Override
