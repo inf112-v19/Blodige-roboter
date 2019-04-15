eeb3854f60013e598843e80d5fd59b1fd6ed3ecb
Daniel Berge <daniel@berge.us>
2019-04-15 12:29:11 +0200

    Removed schedulesync

--- src/main/java/no/uib/inf112/core/ui/UIHandler.java
+++ src/main/java/no/uib/inf112/core/ui/UIHandler.java
@@ -151,21 +151,19 @@ public class UIHandler implements Disposable {
             });
         }
 
-        GameGraphics.scheduleSync(() -> {
-            int flags = GameGraphics.getRoboRally().getPlayerHandler().getFlagCount();
+        int flags = GameGraphics.getRoboRally().getPlayerHandler().getFlagCount();
 
-            HorizontalGroup flagsTaken = new HorizontalGroup().space(DEFAULT_SPACING).padRight(DEFAULT_SPACING);
-            topRow.add(flagsTaken);
-            for (int i = 0; i < flags; i++) {
-                int id = i;
-                flagsTaken.addActor(new ControlPanelElement(FLAG_TAKEN_TEXTURE) {
-                    @Override
-                    public boolean isDisabled() {
-                        return GameGraphics.getRoboRally().getPlayerHandler().mainPlayer().getFlags() <= id;
-                    }
-                });
-            }
-        }, 0);
+        HorizontalGroup flagsTaken = new HorizontalGroup().space(DEFAULT_SPACING).padRight(DEFAULT_SPACING);
+        topRow.add(flagsTaken);
+        for (int i = 0; i < flags; i++) {
+            int id = i;
+            flagsTaken.addActor(new ControlPanelElement(FLAG_TAKEN_TEXTURE) {
+                @Override
+                public boolean isDisabled() {
+                    return GameGraphics.getRoboRally().getPlayerHandler().mainPlayer().getFlags() <= id;
+                }
+            });
+        }
 
         //display power button, it will by default be to the right
         powerButton = new PowerButton();
