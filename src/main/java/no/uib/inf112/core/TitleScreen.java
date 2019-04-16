c45d6daeb5e23dc977db7d7aadbd20ee8c837bf5
rikkeaas <rikrikaas@gmail.com>
2019-04-16 18:47:43 +0200

    Added play 'button' and finally managed to center it when resizing window

--- src/main/java/no/uib/inf112/core/TitleScreen.java
+++ src/main/java/no/uib/inf112/core/TitleScreen.java
@@ -10,7 +10,10 @@ import com.badlogic.gdx.graphics.Texture;
 
 public class TitleScreen implements Screen {
 
-    private Texture header;
+    private Texture header = new Texture("header.png");
+    private Texture play_on = new Texture("play_on.png");
+    private Texture play_off = new Texture("play_off.png");
+
     private GameGraphics game;
     private OrthographicCamera camera;
 
@@ -19,7 +22,6 @@ public class TitleScreen implements Screen {
 
     public TitleScreen(GameGraphics game) {
         this.game = game;
-        header = new Texture("header.png");
     }
 
     @Override
@@ -27,8 +29,8 @@ public class TitleScreen implements Screen {
         width = Gdx.graphics.getWidth();
         height = Gdx.graphics.getHeight();
 
-        camera = new OrthographicCamera(width, height);
-        camera.position.set(width / 2, height / 2, 0);
+        camera = new OrthographicCamera(width, height);//width * (height / width));
+        camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
         camera.update();
 
 
@@ -36,8 +38,7 @@ public class TitleScreen implements Screen {
 
     @Override
     public void render(float v) {
-//        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT |
-//                (Gdx.graphics.getBufferFormat().coverageSampling ? GL20.GL_COVERAGE_BUFFER_BIT_NV : 0));
+
         Gdx.gl.glClearColor(0.5f, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -45,9 +46,24 @@ public class TitleScreen implements Screen {
             game.setScreen(new GameScreen(game));
         }
 
+        game.batch.setTransformMatrix(camera.view);
+        game.batch.setProjectionMatrix(camera.projection);
+
         game.batch.begin();
 
-        game.batch.draw(header, 0, camera.position.y + (header.getHeight() / 3));
+        game.batch.draw(header, 0, camera.viewportHeight / 2 + header.getHeight() / 3f);
+
+        float playX = -(play_on.getWidth() / 2f);
+        float playY = -(play_on.getHeight() / 2f);
+
+
+        if (Gdx.input.getX() >= width / 2 + playX && Gdx.input.getX() <= width / 2 + playX + play_on.getWidth() &&
+                Gdx.input.getY() >= height / 2 + playY && Gdx.input.getY() <= height / 2 + playY + play_on.getHeight()) {
+
+            game.batch.draw(play_on, camera.viewportWidth / 2 + playX, camera.viewportHeight / 2 + playY);
+        } else {
+            game.batch.draw(play_off, camera.viewportWidth / 2 + playX, camera.viewportHeight / 2 + playY);
+        }
 
         game.batch.end();
 
@@ -55,8 +71,11 @@ public class TitleScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-
+        this.width = width;
+        this.height = height;
+        camera.update();
     }
+
 
     @Override
     public void pause() {
