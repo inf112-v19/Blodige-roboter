3537cc798f00fa5757e82549962d5d73c3f6edd9
Daniel Berge <daniel@berge.us>
2019-04-17 15:36:31 +0200

    Added new EndScreen

---
+++ src/main/java/no/uib/inf112/core/screens/EndScreen.java
@@ -0,0 +1,46 @@
+package no.uib.inf112.core.screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+
+public class EndScreen implements Screen {
+
+    private int width;
+    private int height;
+
+    @Override
+    public void show() {
+        width = Gdx.graphics.getWidth();
+        height = Gdx.graphics.getHeight();
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
