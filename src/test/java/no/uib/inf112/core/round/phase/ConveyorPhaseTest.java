935f48abae7a7214cbf5f2a2688da68aefb692cd
Elg <klink100@gmail.com>
2019-04-17 14:53:05 +0200

    Move conveyor phase tests to new, larger, map

--- src/test/java/no/uib/inf112/core/round/phase/ConveyorPhaseTest.java
+++ src/test/java/no/uib/inf112/core/round/phase/ConveyorPhaseTest.java
@@ -19,7 +19,8 @@ public class ConveyorPhaseTest extends TestGraphics {
 
     @Before
     public void setUp() {
-        roboRally = GameGraphics.createRoboRally(TEST_MAP_FOLDER + File.separatorChar + "conveyor_tile_test_map.tmx", 1);
+        roboRally = GameGraphics
+            .createRoboRally(TEST_MAP_FOLDER + File.separatorChar + "conveyor_complex_rotation_test_map.tmx", 1);
         player = roboRally.getPlayerHandler().testPlayer();
         phase = new ConveyorPhase(0);
     }
@@ -32,130 +33,130 @@ public class ConveyorPhaseTest extends TestGraphics {
 
     @Test
     public void rotateLeftCircleNormal() {
-        player.teleport(1, 8);
+        player.teleport(1, 2);
         player.setDirection(SOUTH);
         runPhase();
 
         assertEquals(1, player.getX());
-        assertEquals(7, player.getY());
+        assertEquals(1, player.getY());
         assertEquals(EAST, player.getDirection());
 
         runPhase();
 
         assertEquals(2, player.getX());
-        assertEquals(7, player.getY());
+        assertEquals(1, player.getY());
         assertEquals(NORTH, player.getDirection());
 
         runPhase();
 
         assertEquals(2, player.getX());
-        assertEquals(8, player.getY());
+        assertEquals(2, player.getY());
         assertEquals(WEST, player.getDirection());
 
         runPhase();
 
         assertEquals(1, player.getX());
-        assertEquals(8, player.getY());
+        assertEquals(2, player.getY());
         assertEquals(SOUTH, player.getDirection());
     }
     @Test
     public void rotateRightCircleNormal() {
-        player.teleport(3, 8);
+        player.teleport(3, 2);
         player.setDirection(EAST);
         runPhase();
 
         assertEquals(4, player.getX());
-        assertEquals(8, player.getY());
+        assertEquals(2, player.getY());
         assertEquals(SOUTH, player.getDirection());
 
         runPhase();
 
         assertEquals(4, player.getX());
-        assertEquals(7, player.getY());
+        assertEquals(1, player.getY());
         assertEquals(WEST, player.getDirection());
 
         runPhase();
 
         assertEquals(3, player.getX());
-        assertEquals(7, player.getY());
+        assertEquals(1, player.getY());
         assertEquals(NORTH, player.getDirection());
 
         runPhase();
 
         assertEquals(3, player.getX());
-        assertEquals(8, player.getY());
+        assertEquals(2, player.getY());
         assertEquals(EAST, player.getDirection());
     }
 
     @Test
     public void rotateLeftCircleExpress() {
-        player.teleport(5, 8);
+        player.teleport(5, 2);
         player.setDirection(SOUTH);
 
         runPhase();
 
         assertEquals(6, player.getX());
-        assertEquals(7, player.getY());
+        assertEquals(1, player.getY());
         assertEquals(NORTH, player.getDirection());
 
         runPhase();
 
         assertEquals(5, player.getX());
-        assertEquals(8, player.getY());
+        assertEquals(2, player.getY());
         assertEquals(SOUTH, player.getDirection());
     }
 
     @Test
     public void rotateLeftCircleExpress2() {
-        player.teleport(5, 7);
+        player.teleport(5, 1);
         player.setDirection(WEST);
 
         runPhase();
 
         assertEquals(6, player.getX());
-        assertEquals(8, player.getY());
+        assertEquals(2, player.getY());
         assertEquals(EAST, player.getDirection());
 
         runPhase();
 
         assertEquals(5, player.getX());
-        assertEquals(7, player.getY());
+        assertEquals(1, player.getY());
         assertEquals(WEST, player.getDirection());
     }
 
     @Test
     public void rotateRightCircleExpress() {
-        player.teleport(7, 8);
+        player.teleport(7, 2);
         player.setDirection(EAST);
 
         runPhase();
 
         assertEquals(8, player.getX());
-        assertEquals(7, player.getY());
+        assertEquals(1, player.getY());
         assertEquals(WEST, player.getDirection());
 
         runPhase();
 
         assertEquals(7, player.getX());
-        assertEquals(8, player.getY());
+        assertEquals(2, player.getY());
         assertEquals(EAST, player.getDirection());
     }
 
     @Test
     public void rotateRightCircleExpress2() {
-        player.teleport(7, 7);
+        player.teleport(7, 1);
         player.setDirection(NORTH);
 
         runPhase();
 
         assertEquals(8, player.getX());
-        assertEquals(8, player.getY());
+        assertEquals(2, player.getY());
         assertEquals(SOUTH, player.getDirection());
 
         runPhase();
 
         assertEquals(7, player.getX());
-        assertEquals(7, player.getY());
+        assertEquals(1, player.getY());
         assertEquals(NORTH, player.getDirection());
     }
 }
