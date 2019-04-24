eb9d27b52c5a386f807214fd9f156bbe46bf136a
Kristian Aksland <K.Aksland@student.uib.no>
2019-04-24 16:51:36 +0200

    Added position check for players in playerphase check

--- src/test/java/no/uib/inf112/core/round/phase/PlayerPhaseTest.java
+++ src/test/java/no/uib/inf112/core/round/phase/PlayerPhaseTest.java
@@ -4,6 +4,7 @@ import no.uib.inf112.core.GameGraphics;
 import no.uib.inf112.core.RoboRally;
 import no.uib.inf112.core.player.IPlayer;
 import no.uib.inf112.core.util.Direction;
+import no.uib.inf112.core.util.Vector2Int;
 import no.uib.inf112.desktop.TestGraphics;
 import org.junit.Before;
 import org.junit.Test;
@@ -24,15 +25,21 @@ public class PlayerPhaseTest extends TestGraphics {
         player0 = roboRally.getPlayerHandler().testPlayer();
         player1 = roboRally.getPlayerHandler().getPlayers().get(1);
     }
-    
+
     @Test
     public void runningPhaseShouldRotateBothPlayers5Times() {
+        Vector2Int player0Pos = new Vector2Int(player0.getX(), player0.getY());
+        Vector2Int player1Pos = new Vector2Int(player1.getX(), player1.getY());
         Direction player1Dir = player1.getDirection();
         Direction player0Dir = player0.getDirection();
+
         PlayerPhase playerPhase = new PlayerPhase(0);
         playerPhase.startPhase(roboRally.getCurrentMap());
+
         assertEquals(player0Dir.turnRight(), player0.getDirection());
+        assertEquals(player0Pos, new Vector2Int(player0.getX(), player0.getY()));
         assertEquals(player1Dir.turnRight(), player1.getDirection());
+        assertEquals(player1Pos, new Vector2Int(player1.getX(), player1.getY()));
     }
 
 }
