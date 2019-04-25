e2e5e39136684e62fa5a165b90fc264ca753a008
rikkeaas <rikrikaas@gmail.com>
2019-04-25 16:44:43 +0200

    Fixing Join screen name entering

--- src/main/java/no/uib/inf112/core/screens/JoinScreen.java
+++ src/main/java/no/uib/inf112/core/screens/JoinScreen.java
@@ -1,7 +1,11 @@
 package no.uib.inf112.core.screens;
 
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.Align;
 import no.uib.inf112.core.GameGraphics;
 
 
@@ -20,9 +24,22 @@ public class JoinScreen extends AbstractMenuScreen {
 
         TextField.TextFieldStyle textFieldStyle = new TextField.TextFieldStyle();
         textFieldStyle.font = game.generateFont("screen_font.ttf", 50);
-        textFieldStyle.fontColor = Color.BLACK;
-        //textFieldStyle.disabledFontColor
-        //TextField nameField = new TextField("", )
+        textFieldStyle.fontColor = Color.WHITE;
+
+        Pixmap myPixMap = new Pixmap(1, 1, Pixmap.Format.RGBA8888);
+        myPixMap.setColor(1, 1, 1, 0.5f);
+        myPixMap.fillRectangle(0, 0, 1, 1);
+        textFieldStyle.cursor = new TextureRegionDrawable(new Texture(myPixMap));
+        textFieldStyle.disabledFontColor = Color.GRAY;
+        textFieldStyle.background = new TextureRegionDrawable(new Texture(myPixMap));
+        TextField nameField = new TextField("Enter name", textFieldStyle);
+        nameField.setWidth(stage.getWidth() / 3);
+        nameField.setAlignment(Align.center);
+        nameField.setMaxLength(13);
+        nameField.setPosition(stage.getWidth() / 2 - nameField.getWidth() / 2, stage.getHeight() / 2 - nameField.getHeight() / 2);
+
+        stage.addActor(nameField);
+
 
     }
 }
