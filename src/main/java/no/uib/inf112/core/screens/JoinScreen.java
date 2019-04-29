67ed9050b1a4c9026951ef85248cafe39c45b4fc
rikkeaas <rikrikaas@gmail.com>
2019-04-29 13:03:06 +0200

    Made join button disabled if port is invalid for join screen

--- src/main/java/no/uib/inf112/core/screens/JoinScreen.java
+++ src/main/java/no/uib/inf112/core/screens/JoinScreen.java
@@ -1,5 +1,6 @@
 package no.uib.inf112.core.screens;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import no.uib.inf112.core.GameGraphics;
@@ -8,6 +9,8 @@ import no.uib.inf112.core.GameGraphics;
 public class JoinScreen extends AbstractMenuScreen {
 
     private GameGraphics game;
+    TextButton joinButton;
+    TextField portField;
 
     public JoinScreen(GameGraphics game) {
         super(game);
@@ -19,13 +22,15 @@ public class JoinScreen extends AbstractMenuScreen {
         TextButton returnButton = createReturnButton(70);
         returnButton.setPosition(stage.getWidth() / 2 - returnButton.getWidth() - 10, stage.getHeight() / 20);
 
-        TextButton joinButton = createButton("JOIN", 70);
+        joinButton = createButton("JOIN", 70);
+        joinButton.getStyle().disabledFontColor = Color.GRAY;
+        joinButton.setDisabled(true);
         joinButton.setPosition(stage.getWidth() / 2 + 10, stage.getHeight() / 20);
 
         TextField nameField = createInputField("Enter name", 13);
         nameField.setPosition(stage.getWidth() / 2 - nameField.getWidth() / 2, stage.getHeight() / 2 + nameField.getHeight() / 2);
 
-        TextField portField = createDigitInputField("Enter port", 5);
+        portField = createDigitInputField("Enter port", 5);
         portField.setPosition(stage.getWidth() / 2 - portField.getWidth() / 2, stage.getHeight() / 2 - portField.getHeight());
 
 
@@ -33,6 +38,23 @@ public class JoinScreen extends AbstractMenuScreen {
         stage.addActor(joinButton);
         stage.addActor(nameField);
         stage.addActor(portField);
+    }
+
+    @Override
+    public void render(float v) {
+        super.render(v);
+
+        int portNb;
+        try {
+            portNb = Integer.parseInt(portField.getText().toString());
+        } catch (NumberFormatException e) {
+            portNb = -1;
+        }
+        if (49152 <= portNb && 65535 >= portNb) {
+            joinButton.setDisabled(false);
+        } else {
+            joinButton.setDisabled(true);
+        }
     }
 
 }
