549ba4b41d275770199821fc85e3e3dc1bca453b
Daniel Berge <daniel@berge.us>
2019-05-01 16:05:44 +0200

    Added players/out of players i host screeen

--- src/main/java/no/uib/inf112/core/screens/HostLobbyScreen.java
+++ src/main/java/no/uib/inf112/core/screens/HostLobbyScreen.java
@@ -13,16 +13,16 @@ import java.net.*;
 public class HostLobbyScreen extends LobbyScreen {
     private String ipAddress;
     private GameGraphics game;
+    private int port;
 
     HostLobbyScreen(GameGraphics game, boolean isHost, String ip, int port) throws IOException {
         super(game, isHost, ip, port);
+        this.port = port;
         this.game = game;
         try (final DatagramSocket socket = new DatagramSocket()) {
             socket.connect(InetAddress.getByName("8.8.8.8"), 10002);
             ipAddress = socket.getLocalAddress().getHostAddress();
-            if (ipAddress.equals("0.0.0.0")) {
-                ipAddress = Inet4Address.getLocalHost().getHostAddress();
-            }
+            if (ipAddress.equals("0.0.0.0")) ipAddress = Inet4Address.getLocalHost().getHostAddress();
         } catch (UnknownHostException e) {
             e.printStackTrace();
         } catch (SocketException e) {
@@ -33,8 +33,7 @@ public class HostLobbyScreen extends LobbyScreen {
     @Override
     public void show() {
         super.show();
-
-        Label label = game.createLabel("IP: " + ipAddress, stage.getWidth() / 2 - stage.getWidth() / 4, stage.getHeight() - 55, 50);
+        Label ipPortLabel = game.createLabel("IP: " + ipAddress + " Port: " + port, stage.getWidth() / 2 - stage.getWidth() / 4, stage.getHeight() - 70, 50);
         TextButton startButton = createButton("START", 80);
         startButton.addListener(new ClickListener() {
             @Override
@@ -44,8 +43,18 @@ public class HostLobbyScreen extends LobbyScreen {
             }
         });
         startButton.setPosition(3 * stage.getWidth() / 4 + 20, stage.getHeight() / 20);
-        stage.addActor(label);
+        stage.addActor(ipPortLabel);
         stage.addActor(startButton);
         stage.addActor(new Actor());
+        stage.addActor(new Actor());
+    }
+
+    @Override
+    public void render(float v) {
+        super.render(v);
+        //TODO Render connectedplayers out of max players EX: 5/7
+        int length = client.getPlayerNames().length;
+        Label players = game.createLabel(length + "/" + GameGraphics.players, stage.getWidth() / 2, stage.getHeight() - 200, 50);
+        stage.addActor(players);
     }
 }
